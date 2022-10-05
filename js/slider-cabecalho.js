0c30f7111deaa426539fa9c4fb5d2c7aba068b83
Jefferson Honorato <jeffhonoratobarros@gmail.com>
2022-10-05 10:24:10 -0300

    Ajustes

---
+++ js/slider-cabecalho.js
@@ -0,0 +1,29 @@
+let tempo = 3000;
+let imagemIndex = 0;
+let imagens = document.querySelectorAll(".banner__img");
+let maxImagens = imagens.length;
+
+function nextImagem() {
+
+
+    imagens[imagemIndex].classList.remove("selected");
+
+    imagemIndex++;
+
+    if(imagemIndex >= maxImagens) {
+        imagemIndex = 0;
+    }
+
+    imagens[imagemIndex].classList.add("selected");
+}
+
+
+function start() {
+    setInterval(() => {
+        // Troca de imagem
+        nextImagem();
+    }, tempo);
+};
+
+
+window.addEventListener("load", start);
