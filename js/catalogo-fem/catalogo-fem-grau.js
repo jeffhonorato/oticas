8669b70859320aaa2ccb1bb697a7c3e61978c04d
Jefferson Honorato <53011109+jeffhonorato@users.noreply.github.com>
2022-10-10 09:46:53 -0300

    Ajustes

--- js/catalogo-fem/catalogo-fem-grau.js
+++ js/catalogo-fem/catalogo-fem-grau.js
@@ -1,1 +1,8 @@
-const btn = document.querySelector(".js-btn-card1-grauFem");
+const imgPeq = document.querySelectorAll(".js-imgPeq-card1-grau");
+    imgPeq.forEach((img) => {
+        img.addEventListener("click", () => {
+            const imgGra = document.querySelector(".js-imgGra-card1-grau");
+            imgGra.src = img.src;
+        })
+    })
+
