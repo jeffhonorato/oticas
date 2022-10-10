07de1b5f21088ace14d4e29c8e00a00680c06da9
Jefferson Honorato <jeffhonoratobarros@gmail.com>
2022-10-10 15:12:20 -0300

    Ajustes

--- js/catalogo-fem/catalogo-fem-grau.js
+++ js/catalogo-fem/catalogo-fem-grau.js
@@ -9,22 +9,64 @@ const modal = () => {
     });
 
     const btnDetalhesCard1 = document.querySelector(".js-btn-card1-grauFem");
-        btnDetalhesCard1.addEventListener("click", (e) => {
-            const modalBox = document.querySelector(".js-modal");
-            modalBox.classList.add("ativo");
-            e.preventDefault();
-        });  
+    const modalBox1 = document.querySelector(".js-modal");
+    btnDetalhesCard1.addEventListener("click", (e) => {
+        modalBox1.classList.add("ativo");
+        e.preventDefault();
 
+        
+    });  
 
-    const btnFechar = document.querySelector(".btn-fechar");
-    btnFechar.addEventListener("click", () => {
-        const modalBox = document.querySelector(".js-modal");
-        modalBox.classList.remove("ativo");
-    }); 
+    console.log(modalBox1);
+
+    // const btnFechar = document.querySelector(".btn-fechar");
+    // btnFechar.addEventListener("click", (e) => {
+    //     const modalBox1 = document.querySelector(".js-modal");
+    //     modalBox1.classList.remove("ativo");
+    //     console.log(modalBox1);
+        
+    // }); 
+
     
-    console.log(btnFechar);
+    
+   
+};
 
+const modal2 = () => {
+
+    const imgPeq = document.querySelectorAll(".js-imgPeq-card2-grau");
+    imgPeq.forEach((img) => {
+        img.addEventListener("click", () => {
+            const imgGra = document.querySelector(".js-imgGra-card2-grau");
+            imgGra.src = img.src;
+        });
+    });
+
+    const btnDetalhesCard2 = document.querySelector(".js-btn-card2-grauFem");
+    
+    btnDetalhesCard2.addEventListener("click", (e) => {
+        const modalBox2 = document.querySelector(".js-modal-2");
+        modalBox2.classList.add("ativo");
+        e.preventDefault();
+
+        
+    });  
+
+    console.log(modalBox1);
+
+    // const btnFechar = document.querySelector(".btn-fechar");
+    // btnFechar.addEventListener("click", (e) => {
+    //     const modalBox1 = document.querySelector(".js-modal");
+    //     modalBox1.classList.remove("ativo");
+    //     console.log(modalBox1);
+        
+    // }); 
+
+    
+    
+   
 };
 
  
 modal();    
+modal2();    
