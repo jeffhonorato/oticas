6c06ad18dc2db31d1c9f0c40054dfc0b0897482a
Jefferson Honorato <jeffhonoratobarros@gmail.com>
2022-10-10 15:33:22 -0300

    Ajustes

--- js/catalogo-fem/catalogo-fem-grau.js
+++ js/catalogo-fem/catalogo-fem-grau.js
@@ -9,64 +9,22 @@ const modal = () => {
     });
 
     const btnDetalhesCard1 = document.querySelector(".js-btn-card1-grauFem");
-    const modalBox1 = document.querySelector(".js-modal");
     btnDetalhesCard1.addEventListener("click", (e) => {
+        const modalBox1 = document.querySelector(".js-modal");
         modalBox1.classList.add("ativo");
-        e.preventDefault();
-
-        
+        e.preventDefault();        
     });  
 
-    console.log(modalBox1);
+    console.log(btnDetalhesCard1)
 
-    // const btnFechar = document.querySelector(".btn-fechar");
-    // btnFechar.addEventListener("click", (e) => {
-    //     const modalBox1 = document.querySelector(".js-modal");
-    //     modalBox1.classList.remove("ativo");
-    //     console.log(modalBox1);
-        
-    // }); 
-
-    
-    
-   
+    const btnFechar = document.querySelector(".btn-fechar");
+    btnFechar.addEventListener("click", (e) => {
+        const modalBox1 = document.querySelector(".js-modal");
+        modalBox1.classList.remove("ativo");    
+    }); 
 };
 
-const modal2 = () => {
-
-    const imgPeq = document.querySelectorAll(".js-imgPeq-card2-grau");
-    imgPeq.forEach((img) => {
-        img.addEventListener("click", () => {
-            const imgGra = document.querySelector(".js-imgGra-card2-grau");
-            imgGra.src = img.src;
-        });
-    });
-
-    const btnDetalhesCard2 = document.querySelector(".js-btn-card2-grauFem");
-    
-    btnDetalhesCard2.addEventListener("click", (e) => {
-        const modalBox2 = document.querySelector(".js-modal-2");
-        modalBox2.classList.add("ativo");
-        e.preventDefault();
-
-        
-    });  
-
-    console.log(modalBox1);
-
-    // const btnFechar = document.querySelector(".btn-fechar");
-    // btnFechar.addEventListener("click", (e) => {
-    //     const modalBox1 = document.querySelector(".js-modal");
-    //     modalBox1.classList.remove("ativo");
-    //     console.log(modalBox1);
-        
-    // }); 
-
-    
-    
-   
-};
 
  
 modal();    
-modal2();    
+ 
