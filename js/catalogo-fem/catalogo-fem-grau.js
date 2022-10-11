d4bbf20ad0997390c297ccafb0fbac882bc0b115
Jefferson Honorato <53011109+jeffhonorato@users.noreply.github.com>
2022-10-11 15:35:33 -0300

    ajustes

--- js/catalogo-fem/catalogo-fem-grau.js
+++ js/catalogo-fem/catalogo-fem-grau.js
@@ -1,30 +1,8 @@
-const modal = () => {
+const imgPeq = document.querySelectorAll(".js-imgPeq-card1-grau");
+const imgGra = document.querySelector(".js-imgGra-card1-grau");
 
-    const imgPeq = document.querySelectorAll(".js-imgPeq-card1-grau");
     imgPeq.forEach((img) => {
         img.addEventListener("click", () => {
-            const imgGra = document.querySelector(".js-imgGra-card1-grau");
             imgGra.src = img.src;
         });
     });
-
-    const btnDetalhesCard1 = document.querySelector(".js-btn-card1-grauFem");
-        btnDetalhesCard1.addEventListener("click", (e) => {
-            const modalBox = document.querySelector(".js-modal");
-            modalBox.classList.add("ativo");
-            e.preventDefault();
-        });  
-
-
-    const btnFechar = document.querySelector(".btn-fechar");
-    btnFechar.addEventListener("click", () => {
-        const modalBox = document.querySelector(".js-modal");
-        modalBox.classList.remove("ativo");
-    }); 
-    
-    console.log(btnFechar);
-
-};
-
- 
-modal();    
