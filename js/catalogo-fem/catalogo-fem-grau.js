b64f18b9bb65e32d2307b10912c6d1ce1acb0a49
Jefferson Honorato <jeffhonoratobarros@gmail.com>
2023-03-31 15:22:52 -0300

    Ajustes

--- js/catalogo-fem/catalogo-fem-grau.js
+++ js/catalogo-fem/catalogo-fem-grau.js
@@ -21,9 +21,22 @@ const card2 = () => {
 };
 
 
+const card3 = () => {
+    const imgPeq1 = document.querySelectorAll(".js-cardGrau-imgP-3");
+    const imgGra1 = document.querySelector(".js-cardGrau-imgG-3");
+    
+    imgPeq1.forEach((img) => {
+        img.addEventListener("click", () => {
+            imgGra1.src = img.src;
+        });
+    });
+};
+
+
 
    
 
 
 card1();
 card2();
+card3();
